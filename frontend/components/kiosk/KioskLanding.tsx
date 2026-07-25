9ad926cce6d5061579e9b587f8af4803f2bc4d9f
Satyam <kumarsatyam01303@gmail.com>
2026-07-25 13:23:06 +0530

    country flag

--- frontend/components/kiosk/KioskLanding.tsx
+++ frontend/components/kiosk/KioskLanding.tsx
@@ -14,13 +14,13 @@ const HERO_DESTINATIONS = [
 ] as const;
 
 const DESTINATIONS = [
-  { name: "Canada", cardName: "Canada", code: "CA", universities: "125+ universities", accent: "red", image: "/country-images/canada-2.jpeg", imagePosition: "center", flagImage: "/flags/canada-flag.avif" },
-  { name: "United Kingdom", cardName: "UK", code: "UK", universities: "160+ universities", accent: "blue", image: "/country-images/uk.avif", imagePosition: "center", flagImage: "/flags/uk-flag.avif" },
-  { name: "Australia", cardName: "Australia", code: "AU", universities: "43 universities", accent: "gold", image: "/country-images/australia.avif", imagePosition: "center", flagImage: "/flags/australia-flag.jpg" },
+  { name: "Canada", cardName: "Canada", code: "CA", universities: "125+ universities", accent: "red", image: "/country-images/canada-attached.jpeg", imagePosition: "center", flagImage: "/flags/canada-flag.avif" },
+  { name: "United Kingdom", cardName: "UK", code: "UK", universities: "160+ universities", accent: "blue", image: "/country-images/united-kingdom-attached.jpeg", imagePosition: "center", flagImage: "/flags/uk-flag.avif" },
+  { name: "Australia", cardName: "Australia", code: "AU", universities: "43 universities", accent: "gold", image: "/country-images/australia.avif", imagePosition: "center", flagImage: "/flags/australia.png" },
   { name: "United States", cardName: "USA", code: "US", universities: "4,000+ institutions", accent: "navy", image: "/country-images/usa.jpg", imagePosition: "center", flagImage: "/flags/usa-flag.jpg" },
-  { name: "New Zealand", cardName: "New Zealand", code: "NZ", universities: "8 universities", accent: "aqua", image: "/country-images/new-zealand.png", imagePosition: "center", flagImage: "/flags/new-zealand-flag.avif" },
-  { name: "Germany", cardName: "Germany", code: "DE", universities: "400+ institutions", accent: "amber", image: "/country-images/germany.png", imagePosition: "center", flagImage: "/flags/germany-flag.webp" },
-  { name: "Ireland", cardName: "Ireland", code: "IE", universities: "28 institutions", accent: "green", image: "/country-images/ireland.png", imagePosition: "center", flagImage: "/flags/ireland-flag.webp" },
+  { name: "New Zealand", cardName: "New Zealand", code: "NZ", universities: "8 universities", accent: "aqua", image: "/country-images/new-zealand-attached.jpeg", imagePosition: "center", flagImage: "/flags/new-zealand.png" },
+  { name: "Germany", cardName: "Germany", code: "DE", universities: "400+ institutions", accent: "amber", image: "/country-images/germany-attached.jpeg", imagePosition: "center", flagImage: "/flags/germany.png" },
+  { name: "Cyprus", cardName: "Cyprus", code: "CY", universities: "28 institutions", accent: "green", image: "/country-images/ireland.png", imagePosition: "center", flagImage: "/flags/cyprus.png" },
   { name: "France", cardName: "France", code: "FR", universities: "250+ institutions", accent: "indigo", image: "/country-images/france.png", imagePosition: "center", flagImage: "/flags/france-flag.jpg" },
   { name: "United Arab Emirates", cardName: "UAE", code: "AE", universities: "70+ institutions", accent: "emerald", image: "/country-images/uae.jpg", imagePosition: "center", flagImage: "/flags/uae-flag.jpg" },
 ] as const;
@@ -124,7 +124,7 @@ export function KioskLanding() {
             <button className={`edu-kiosk-country-card accent-${destination.accent}`} type="button" onClick={() => setSelected(destination)} key={destination.name}>
               <span className="edu-kiosk-country-image">
                 <Image src={destination.image} alt={`Study destination in ${destination.name}`} fill sizes="(max-width: 620px) 110px, (max-width: 820px) 210px, 420px" style={{ objectPosition: "imagePosition" in destination ? destination.imagePosition : "center" }} />
-                <span className="edu-kiosk-flag" role="img" aria-label={`${destination.name} flag`}>
+                <span className={`edu-kiosk-flag flag-${destination.code.toLowerCase()}`} role="img" aria-label={`${destination.name} flag`}>
                   <Image src={destination.flagImage} alt="" fill sizes="(max-width: 620px) 34px, 82px" />
                 </span>
                 <span className="edu-kiosk-country-overlay">
